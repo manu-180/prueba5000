7f9ec3f1862bc02e8129c721a5323e2a0f488418
manu-180 <manumanu97@hotmail.com>
2024-05-25 21:28:34 -0300

    api

--- prueba_5000/prueba_5000.py
+++ prueba_5000/prueba_5000.py
@@ -12,6 +12,26 @@ def index() -> rx.Component:
         )
     )
     
+
+async def hola(user:str) -> str:
+    if user == "juli":
+        return "hello juli "
+    elif user == "cami":
+        return "hello cami"
+    elif user == "theo":
+        return "hello theo"
+    elif user == "viejo":
+        return "hello viejo"
+    elif user == "vieja":
+        return "hello vieja"
+
+async def hola():
+    return "hello manu"
+    
+    
     
 app = rx.App()
 app.add_page(index)
+
+app.api.add_api_route("/hola/{user}", hola)
+app.api.add_api_route("/hola", hola)
