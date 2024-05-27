6d86e15661894c65141b7850711890f0a11b5103
manu-180 <manumanu97@hotmail.com>
2024-05-26 21:11:00 -0300

    se empieza

--- prueba_5000/prueba_5000.py
+++ prueba_5000/prueba_5000.py
@@ -1,4 +1,49 @@
 import reflex as rx
+import os
+from supabase import create_client, Client
+
+url: str = "https://gcjyhrlcftbkeaiqlzlm.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjanlocmxjZnRia2VhaXFsemxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU3MjQ3NzAsImV4cCI6MjAzMTMwMDc3MH0.MFsm9DJ9XnVnsTUK-N2SsCBf8wnhW03mGp5d2Z2Jf9Q"
+
+class Horarios(rx.Base):
+    id = int
+    horario = int
+    cant_users = int
+    
+
+class Supabase():
+    
+    supabase: Client = create_client(url, key)
+    
+    def data(self):
+        
+        data_list = []
+        
+        response = self.supabase.table('Horarios').select("*").execute()
+        if len(response.data) > 0:
+            for i in response.data:
+                data_list.append(Horarios(id=i["id"], horario= i["horario"], cant_users=["cant_users"]))
+        return data_list
+    
+    def horarios(self):
+        horarios=[]
+        data = self.data()
+        for i in data:
+            horarios.append(i.horario)
+        horarios.sort()
+        return horarios
+                
+    def unico_horario(self, id):
+        data = self.data()
+        for i in data:
+            if i.id == id:
+                return i.horario
+            
+        
+    
+supabase = Supabase()
+    
+
 
 class Colores(rx.State):
     color:str = "pink"
@@ -13,7 +58,7 @@ class Colores(rx.State):
 def index() -> rx.Component:
     return rx.center(
             rx.button(
-            rx.text("linkedIn"),
+            rx.text(supabase.unico_horario(1)),
             color_scheme=Colores.color,
             on_click=Colores.change_color()
             ),
@@ -23,6 +68,13 @@ def index() -> rx.Component:
         )
     )
     
+    
+def button(id):
+    rx.button(
+        rx.text(supabase.unico_horario(id)),
+        color_scheme=Colores.color,
+        on_click=Colores.change_color()
+    )
 
 async def hola(user:str) -> str:
     if user == "juli":
