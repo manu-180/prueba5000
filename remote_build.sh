087102d8b0b97a21a3a6cc70eef81ef619c70fd8
manu-180 <157671390+manu-180@users.noreply.github.com>
2024-05-28 16:16:04 -0300

    Update remote_build.sh

--- remote_build.sh
+++ remote_build.sh
@@ -1,3 +1,4 @@
+cd prueba_5000
 python -m venv entorno_con_3.11
  .\entorno_con_3.11\Scripts\Activate
 C:\Users\Manuel\Desktop\Folder\x\entorno_con_3.11\Scripts\python.exe -m pip install --upgrade pippip install -r requirements.txt
