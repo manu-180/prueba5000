2115120cd1cefaf930707c0558297d2615ea5696
manu-180 <manumanu97@hotmail.com>
2024-05-27 22:38:29 -0300

    d

--- rxconfig.py
+++ rxconfig.py
@@ -2,10 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="prueba_5000",
-    api_url="https://api.baackend.com",
-    cors_allowed_origins=[
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "https://baackend.com"
-    ]
+    api_url="https://api.baackend.com"
 )
